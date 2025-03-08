aae50f906b75c1bb8107f94d96785567dc613b27
vedangrajoriya <vedangrajoriya@gmail.com>
2025-03-08 15:35:52 +0530

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseUrl = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFjeXdwbGZ2end4Y2F0b2ZpemR4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDE0MTAzMzMsImV4cCI6MjA1Njk4NjMzM30.FzkYxtycv_zAB0HDTKdI_gp7hxYKIz10E_LrW7nrxKI';
+const supabaseAnonKey = 'https://qcywplfvzwxcatofizdx.supabase.co';
 
 if (!supabaseUrl || !supabaseAnonKey) {
   throw new Error(
